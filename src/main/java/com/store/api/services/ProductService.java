ccfc33ff8045f700ad272a2c0f2b5628348713df
Aktan A <aktan.amanaliev@gmail.com>
2024-05-16 12:45:45 +0600

    Add product service and repo

---
+++ src/main/java/com/store/api/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.store.api.services;
+
+import com.store.api.models.Product;
+import com.store.api.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    public void addNewProduct(Product product) {
+        System.out.println(product);
+    }
+}
