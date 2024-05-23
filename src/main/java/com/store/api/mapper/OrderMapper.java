d4ca57bed32059e4fc65905bd504b7ffece04d5f
Aktan A <aktan.amanaliev@gmail.com>
2024-05-23 11:00:39 +0600

    Add mappers

---
+++ src/main/java/com/store/api/mapper/OrderMapper.java
@@ -0,0 +1,23 @@
+package com.store.api.mapper;
+
+import com.store.api.dto.OrderDto;
+import com.store.api.model.Order;
+import org.aspectj.weaver.ast.Or;
+
+public class OrderMapper {
+    public static OrderDto convertEntityToDto(Order order) {
+        OrderDto orderDto = new OrderDto();
+        orderDto.setId(order.getId());
+        orderDto.setStatus(order.getStatus());
+        orderDto.setCreatedAt(order.getCreatedAt());
+        return orderDto;
+    }
+
+    public static Order convertDtoToEntity(OrderDto orderDto) {
+        Order order = new Order();
+        order.setId(orderDto.getId());
+        order.setStatus(orderDto.getStatus());
+        order.setCreatedAt(orderDto.getCreatedAt());
+        return order;
+    }
+}
